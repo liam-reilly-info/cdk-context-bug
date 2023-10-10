93a47705be54ce8948e26ade10712283ab3845e2
Liam Reilly <liam@alchemistlabs.tech>
2023-10-10 10:03:35 +0100

    Initial commit

---
+++ test/cdk-context-bug.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkContextBug from '../lib/cdk-context-bug-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-context-bug-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkContextBug.CdkContextBugStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
